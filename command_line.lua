55fcfa5f800dbbcf8fc0a3d1685f517ca68a98de
XuJiandong <xjd@qn-test-server.it.163.org>
2012-04-09 18:53:15 +0800

    add comman line, from LuaRocks

---
+++ command_line.lua
@@ -0,0 +1,40 @@
+
+
+local cl = {}
+
+-- from LuaRocks 
+--- Extract flags from an arguments list.
+-- Given string arguments, extract flag arguments into a flags set.
+-- For example, given "foo", "--tux=beep", "--bla", "bar", "--baz",
+-- it would return the following:
+-- {["bla"] = true, ["tux"] = "beep", ["baz"] = true}, "foo", "bar".
+function cl.parse_flags(...)
+   local args = {...}
+   local flags = {}
+   for i = #args, 1, -1 do
+      local flag = args[i]:match("^%-%-(.*)")
+      if flag then
+         local var,val = flag:match("([a-z_%-]*)=(.*)")
+         if val then
+            flags[var] = val
+         else
+            flags[flag] = true
+         end
+         table.remove(args, i)
+      end
+   end
+   return flags, unpack(args)
+end
+
+
+function cl.test()
+    local flags, o1, o2 = cl.parse_flags("foo", "--tux=beep", "--bla", "bar", "--baz")
+    assert(o1 == "foo")
+    assert(o2 == "bar")
+    assert(flags["tux"] == "beep")
+    assert(flags["bla"] == true)
+    assert(flags["baz"] == true)
+    print("command line test passed")
+end
+
+return cl
--- test.lua
+++ test.lua
@@ -6,3 +6,6 @@ d.test()
 local ff = require("fuzzy_find")
 ff.test()
 
+local cl = require("command_line")
+cl.test()
+
